7bad79e37e8e2706e782e0ede3ae09151a3741d1
Tanmay <minnu.misra@gmail.com>
2021-10-13 13:43:03 +0530

    add FindPermutation

---
+++ Permutations/findPermutation.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+list<int> permute(vector<int>& V, string& str){
+    vector<int> ans;
+    int i, n = V.size();
+    list<int> l;
+    list<int> res;
+
+    sort(V.begin(), V.end());
+
+    for(i=0;i<n;i++){
+        l.push_back(i);
+    }
+
+    for(i=0;i<str.size();i++){
+        //insert the code
+        if(str[i] == '0'){
+            res.push_back(V[l.back()]);
+            l.pop_back();
+        }
+        else{
+            res.push_back(V[l.front()]);
+            l.pop_front();
+        }
+    }
+
+    res.push_back(l.front());
+    return res;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    string s;
+    cin>>s;
+    cout<<"\n"<<"Answer is\n";
+    for(int i : permute(v, s)){
+        cout<<" "<<i<<" ";
+    }
+
+}
